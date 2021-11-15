8cb1ebd47fabc63a7be122eaf25fda8f514323a6
Sadman Sakib Jisan <sadman.sakibjisan@techhack.ca>
2021-11-15 16:48:38 +0600

    color edited

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,19 +1,21 @@
 import DeleteIcon from "@mui/icons-material/Delete";
 import {
   IconButton,
+  MenuItem,
+  Select,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TableHead,
   TableRow,
-  MenuItem,
-  Select,
+  Typography
 } from "@mui/material";
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import taskPriority from "./JSON/Priority.json";
 import taskStatus from "./JSON/Status.json";
-
+import "./Style/ListStyle.css";
+  
 export default function List({ tasks, setTasks }) {
   const handleDelete = (handlerID) => {
     const newTaskList = tasks.filter((task) => task.id !== handlerID);
@@ -31,6 +33,28 @@ export default function List({ tasks, setTasks }) {
   const [details, setDetails] = useState("");
   const [priority, setPriority] = useState("");
   const [status, setStatus] = useState("");
+  // const updateTask = [...tasks, { details, priority, status }];
+  const handleStatusUpdate = (e, id, priority, details) => {
+    const newID = Date.now();
+    let updatedTask = tasks.filter((task) => task.id !== id);
+    updatedTask = [
+      ...updatedTask,
+      { id: newID, details: details, priority: priority, status: e },
+    ];
+    localStorage.setItem("tasks", JSON.stringify(updatedTask));
+    setTasks(updatedTask);
+  };
+
+  const handlePriorityUpdate = (e, id, status, details) => {
+    const newID = Date.now();
+    let updatedTask = tasks.filter((task) => task.id !== id);
+    updatedTask = [
+      ...updatedTask,
+      { id: newID, details: details, priority: e, status: status },
+    ];
+    localStorage.setItem("tasks", JSON.stringify(updatedTask));
+    setTasks(updatedTask);
+  };
 
   return (
     <div>
@@ -38,7 +62,7 @@ export default function List({ tasks, setTasks }) {
         <TableContainer>
           <Table>
             <TableHead>
-              <TableRow>
+              <TableRow className="tableHeader">
                 <TableCell style={{ fontWeight: "bold" }}>Task</TableCell>
                 <TableCell style={{ fontWeight: "bold" }}>Status</TableCell>
                 <TableCell style={{ fontWeight: "bold" }}>Priority</TableCell>
@@ -47,25 +71,33 @@ export default function List({ tasks, setTasks }) {
             </TableHead>
             <TableBody>
               {tasks.map((task) => (
-                <TableRow
+                <TableRow  className="tableRow"
                   style={{
+                    margin: "10px 0",
                     backgroundColor:
                       task.priority === "Normal"
-                        ? "#A3C8AE"
+                        ? "#6C6"
                         : task.priority === "Medium"
-                        ? "#FFFAA0"
+                        ? "#EA0"
                         : task.priority === "Urgent"
-                        ? "#F54040"
-                        : "",
+                        ? "#E21"
+                            : ""
                   }}
                 >
-                  <TableCell>{task.details}</TableCell>
+                  <TableCell><Typography variant="h6" style={{textDecoration: task.status === "Done" ? "line-through" : "none",color:"white"}}>{task.details}</Typography></TableCell>
                   <TableCell>
                     <Select
-                      style={{ width: "100%" }}
+                      style={{ width: "100%",color:"white" }}
                       size="small"
-                      onChange={(e) => setStatus(e.target.value)}
-                      defaultValue={task.status}
+                      value={task.status}
+                      onChange={(e) =>
+                        handleStatusUpdate(
+                          e.target.value,
+                          task.id,
+                          task.priority,
+                          task.details
+                        )
+                      }
                     >
                       {allStatus &&
                         allStatus.map((data, index) => (
@@ -77,10 +109,17 @@ export default function List({ tasks, setTasks }) {
                   </TableCell>
                   <TableCell>
                     <Select
-                      style={{ width: "100%" }}
+                      style={{ width: "100%",color:"white"}}
                       size="small"
-                      onChange={(e) => setPriority(e.target.value)}
-                      defaultValue={task.priority}
+                      value={task.priority}
+                      onChange={(e) =>
+                        handlePriorityUpdate(
+                          e.target.value,
+                          task.id,
+                          task.status,
+                          task.details
+                        )
+                      }
                     >
                       {allPriority &&
                         allPriority.map((data, index) => (
@@ -91,8 +130,11 @@ export default function List({ tasks, setTasks }) {
                     </Select>
                   </TableCell>
                   <TableCell>
-                    <IconButton edge="end">
-                      <DeleteIcon onClick={() => handleDelete(task.id)} />
+                    <IconButton
+                      edge="end"
+                      onClick={() => handleDelete(task.id)}
+                    >
+                      <DeleteIcon style={{color:"white"}}/>
                     </IconButton>
                   </TableCell>
                 </TableRow>
